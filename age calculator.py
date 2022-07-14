82342bff2e12d953cccc25556943314ca767eea0
1alsavvy <ekpefrederick69@gmail.com>
2022-07-14 17:39:05 +0000

    First assignment  on Learning Python

---
+++ age calculator.py
@@ -0,0 +1,20 @@
+Name = input("Enter your name  ")
+print("Wow! " + Name + " is a nice name")
+
+Age = int(input("Please,Enter your age  "))
+
+Year  = 2022
+
+def get_year():
+    return Year - Age
+print("Hello, " + Name + " you were born in the year", get_year())
+
+
+
+
+
+
+
+
+
+
